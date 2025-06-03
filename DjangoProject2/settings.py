4789d2aad5dd6f54abb41564817f85d2e4ede61d
VictorJude046 <164768834+VictorJude046@users.noreply.github.com>
2025-06-04 00:55:54 +0300

    4

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 LOGIN_REDIRECT_URL = "public:index"
 Login_url = "account:login"
